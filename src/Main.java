8b3a90e1d2207bbf6e9204c3097045de3037f38e
Doris <doris.kaambre@gmail.com>
2018-12-08 17:13:44 +0200

    Main võiks olla norm nüüd suht

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -28,6 +26,6 @@ public class Main extends Application {
         aken.show();
 
         valiTest.setOnAction(event -> new ValiTest("Psühhoanalüüs"));
-
+        lisaTest.setOnAction(event -> new LisaTest("Testi lisamine"));
     }
 }
---
+++ src/ValiTest.java
@@ -0,0 +1,6 @@
+public class ValiTest extends PopUp {
+
+    public ValiTest(String ribaNimi) {
+        super(ribaNimi);
+    }
+}
