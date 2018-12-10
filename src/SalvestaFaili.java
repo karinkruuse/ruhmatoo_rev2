b0b47f6bc0dacb164098931388609eebf6923e0a
Karin <karinkruuse98@gmail.com>
2018-12-10 23:03:49 +0200

    Tegin faili salvestamise klassi

---
+++ src/SalvestaFaili.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class SalvestaFaili {
+
+    ArrayList<String> koguTekst;
+
+    public SalvestaFaili(ArrayList<String> tekst) {
+        koguTekst = tekst;
+
+    }
+}
