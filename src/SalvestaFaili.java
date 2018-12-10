7de83653240b8a962a58e39c60be87968df0e392
Karin <karinkruuse98@gmail.com>
2018-12-10 23:14:27 +0200

    Tegin faili salvestamise klassi

--- src/SalvestaFaili.java
+++ src/SalvestaFaili.java
@@ -1,11 +1,26 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class SalvestaFaili {
 
-    ArrayList<String> koguTekst;
-
     public SalvestaFaili(ArrayList<String> tekst) {
-        koguTekst = tekst;
+
+        String failiPath = "resources" + File.separatorChar + "testid" + File.separatorChar;
+        try (BufferedWriter output = new BufferedWriter(new FileWriter(failiPath + "nimed.txt", true))){
+            output.write(tekst.get(0));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedWriter output = new BufferedWriter(new FileWriter(failiPath + tekst.get(0) + ".txt", true))) {
+            for (int i = 1; i < tekst.size(); i++) {
+                output.write(tekst.get(i));
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
